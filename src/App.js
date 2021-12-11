94a4c1ca9d9426ddad0163e67de6e7332c449fd6
Maitreyee Nath <55870039+mnath30@users.noreply.github.com>
2021-12-11 23:25:57 +0530

    feat: Removal of elements from list Added button for deletion of elements from the list

--- src/App.js
+++ src/App.js
@@ -3,20 +3,38 @@ import { useState } from "react";
 
 export default function App() {
   var item;
+  var updatedList = [];
   var [list, setList] = useState([]);
+  var [inpvalue, setInpvalue] = useState(" ");
   const display = (e) => {
-    item = e.target.value;
+    item = e.currentTarget.value;
+    setInpvalue(item);
   };
   const addToList = () => {
     var newList = [...list];
-    newList.push(item);
+    var newitem = {
+      id: list.length + inpvalue,
+      task: inpvalue
+    };
+    newList.push(newitem);
     setList(newList);
+    setInpvalue("");
+  };
+
+  const remove = (key) => {
+    for (let i = 0; i < list.length; i++) {
+      if (list[i].id !== key) {
+        updatedList.push(list[i]);
+      }
+    }
+    setList(updatedList);
   };
 
   return (
     <div className="App">
       <input
         type="text"
+        value={inpvalue}
         className="input1"
         placeholder="Enter Task"
         onChange={display}
@@ -25,8 +43,20 @@ export default function App() {
         Add Task
       </button>
       <div>
-        {list.map((element, index) => {
-          return <li key={index}>{element}</li>;
+        {list.map((element) => {
+          return (
+            <div key={element.id}>
+              <li>{element.task}</li>
+              <button
+                type="button"
+                onClick={() => {
+                  remove(element.id);
+                }}
+              >
+                Remove Item
+              </button>
+            </div>
+          );
         })}
       </div>
     </div>
